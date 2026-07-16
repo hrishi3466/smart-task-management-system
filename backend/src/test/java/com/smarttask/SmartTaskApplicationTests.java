d2140e689a2cebdce8bf61f75b5e0a5efed17dc1
hrishi3466 <hrishi0626@gmail.com>
2026-07-16 22:38:09 +0530

    Initial project setup with Spring Boot and React

---
+++ backend/src/test/java/com/smarttask/SmartTaskApplicationTests.java
@@ -0,0 +1,12 @@
+package com.smarttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
